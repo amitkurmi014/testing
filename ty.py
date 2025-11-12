830b92e7946b4e50b7f0ed7df2b833c752a7f178
amitkurmi014 <155133519+amitkurmi014@users.noreply.github.com>
2025-11-12 17:20:54 +0545

    ready

--- ty.py
+++
@@ -1,9 +0,0 @@
-import requests
-from parsel import Selector
-
-
-for i in range(1,51):
-    r = requests.get(f'https://books.toscrape.com/catalogue/page-{i}.html')
-    response = Selector(text=r.text)
-    
-    
